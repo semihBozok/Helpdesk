86aa9e26f0ee2528a11a0f3361fe11a93a07d742
Semih <90257453+semihBozok@users.noreply.github.com>
2026-07-15 09:49:46 +0000

    add Ignorefiel

--- HelpdeskApi/Program.cs
+++ HelpdeskApi/Program.cs
@@ -17,24 +17,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 
 var tickets = new List<Ticket>
@@ -60,16 +42,26 @@ var tickets = new List<Ticket>
         CreatedAt = DateTime.Now
     }
 };
-app.MapGet("/tickets", () =>
+
+
+app.MapGet("/ticketshigh",() =>
 {
-    return tickets;
+    
+var hightickets = new List<Ticket>(); 
+    foreach (var t in tickets)
+    {
+    
+    if (t.Priority == "High")
+    hightickets.Add(t);
+
+    } 
+
+return hightickets; 
 });
+
 
 app.MapGet("/", () => "Helpdesk API läuft");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
