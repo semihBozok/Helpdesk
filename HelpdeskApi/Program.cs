e064d8d814cb92dc0118813e6aa9efce1317c711
Semih Bozok <semihtr1806@gmail.com>
2026-07-18 23:07:39 +0200

    Docker for persistent data

--- HelpdeskApi/Program.cs
+++ HelpdeskApi/Program.cs
@@ -23,6 +23,16 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider
+        .GetRequiredService<HelpdeskDbContext>();
+
+    await db.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
